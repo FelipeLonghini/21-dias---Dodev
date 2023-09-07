e9254d207bf068ec3a7dc502cfb66546494996ac
Luiz Felipe da Silva Longhini <luizfelipe.dasilvalonghini@gmail.com>
2023-09-07 11:49:07 -0300

    Dia 6

---
+++  Dia 6/indexdia6.js
@@ -0,0 +1,9 @@
+let numero = Number(prompt("Digite seu número inteiro e positivo"))
+
+for(let i = 0; i <= numero; i++){
+    console.log(i)
+}
+
+for(let i = 0; i <= 50; i += 5){
+    console.log(i)
+}
